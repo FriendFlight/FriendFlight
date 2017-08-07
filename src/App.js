9c876aeb84a7b2a8bdfa459998ce0a062f8af7c5
Austin Wheelwright <pyrotechnician25@gmail.com>
2017-08-07 11:07:03 -0600

    initial commit for home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import router from './router';
+import './reset.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <h1>24Flights</h1>
+      <img src="https://cdn.pixabay.com/photo/2017/03/27/12/26/alpine-2178370_960_720.jpg" className="background"/>
+      {router}
       </div>
     );
   }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import "./Home.css";
+
+
+export default class Home extends Component
+{
+  render()
+  {
+    return (
+      <div className="home">
+        <h1 id="title">24Flights</h1>
+      </div>
+    );
+  }
+}
