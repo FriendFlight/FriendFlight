e3aeb35c0e7948bc0e038ab742b650ca2c9ebdcf
adam f <afiggat@gmail.com>
2017-08-10 10:23:04 -0600

    fixed issue API, switch them from home to flight input component, map data is also calling correctly now

--- src/components/Home/FlightInput/FlightInput.js
+++ src/components/Home/FlightInput/FlightInput.js
@@ -10,7 +10,8 @@ constructor() {
                 flightNumNums: '',
                 flightYear: '',
                 flightMonth: '',
-                flightDay: ''
+                flightDay: '',
+                flight:[],
             }
   this.handleFlightNumberChange=this.handleFlightNumberChange.bind(this);
   this.handleFlightDateChange=this.handleFlightDateChange.bind(this);
@@ -34,15 +35,34 @@ constructor() {
 
   }
     sendTripInfo(){
-      
-      if (this.props){
-      this.props.getFlight(`https://api.flightstats.com/flex/schedules/rest/v1/json/flight/${this.state.flightNumLetters}/
-      ${this.state.flightNumNums}/arriving/${this.state.flightYear}/${this.state.flightMonth}/${this.state.flightDay}?appId=
-      ${config.flightStats.appId}&appKey=${config.flightStats.key}`).then(() => {
-        axios.post('/api/flight', {flightNumber: this.state.flightNumLetters+this.state.flightNumNums,
-         currentUserID: this.props.user.id, arrivalDate: this.state.flightWholeDate, 
-         airportAddress: null, arrivalTime: null, offsetHours: null}).then(response => console.log(response.data))})
-    }}
+      axios.get(`https://api.flightstats.com/flex/schedules/rest/v1/json/flight/${this.state.flightNumLetters}/${this.state.flightNumNums}/arriving/${this.state.flightYear}/${this.state.flightMonth}/${this.state.flightDay}?appId=${config.flightStats.appId}&appKey=${config.flightStats.key}`)
+          .then( res=> {
+            this.setState({
+              flight:res.data
+            })
+            console.log("flight", this.state.flight)
+            axios.post('/api/flight', {flightNumber: this.state.flightNumLetters+this.state.flightNumNums,
+            currentUserID: this.props.user.id, arrivalDate: this.state.flightWholeDate, 
+            airportAddress: null, arrivalTime: null, offsetHours: null})
+              .then(response => null)
+            axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${config.google}`)
+            .then(res=>{
+              this.setState({
+                location:res.data.location
+              })
+              console.log("location", this.state.location)
+              console.log("bug", this.state.location.lat, this.state.location.lng, this.state.flight.appendix.airports[1].name)
+              axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${this.state.location.lat},${this.state.location.lng}&destination=${this.state.flight.appendix.airports[1].name}&key=${config.google}`)
+                .then(res=>{
+                  this.setState({
+                    directions:res.data
+                  })
+                  console.log("directions", this.state.directions)
+                })
+            })
+          })
+      }
+    
 
   render()
   {
