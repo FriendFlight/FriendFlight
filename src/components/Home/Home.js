14940a0e37beefc4eeb8339617f878c6e48b1d7a
Jordan Cooper <jordan@cooperplanet.com>
2017-08-17 18:44:32 -0600

    Add a jumping kangaroo while loading

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ import config from '../config'
 import Modal from 'react-modal'
 import { Transition } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
+import $ from 'jquery'
 
 import Login from './Login/Login';
 import FlightInput from './FlightInput/FlightInput';
@@ -24,50 +25,51 @@ import sadKangaroo from './RoominderSadLogo.png'
 import scrollToComponent from 'react-scroll-to-component';
 //Smooth Scroll
 
-  const Logo = styled.img`
+const Logo = styled.img`
       height: 15em;
       margin: 0 auto;
       display: block;
       margin-top: 10vh;
     `
-  const LogoSmall = styled.img`
+const LogoSmall = styled.img`
       height: 50px;
       margin: 0 auto;
       display: block;
-
+      position: absolute;
+      opacity: 0;
     `
-  const SadKangaroo = styled.img`
+const SadKangaroo = styled.img`
       width: 275px;
       margin: 0 auto;
       display: block;
     `
 
-  const Text = styled.text`
+const Text = styled.text`
       margin: 0 auto;
       font-size: 1.25em;
       font-color: #616161;
     `
-  const Padder = styled.div`
+const Padder = styled.div`
       max-width: 90vw;
       margin: 0 auto;
       text-align: center;
     `
-  const Spacer5 = styled.div`
+const Spacer5 = styled.div`
     margin-top: 5vh;
     `
-  const Spacer10 = styled.div`
+const Spacer10 = styled.div`
     margin-top: 10vh;
     `
-  const Spacer10H = styled.div`
+const Spacer10H = styled.div`
     height: 10vh;
     padding-top: 10px;
     `
-  const Spacer12 = styled.div`
+const Spacer12 = styled.div`
     margin-top: 12vh;
     `
 
 
-export default class Home extends Component 
+export default class Home extends Component
 {
   constructor() {
     super()
@@ -79,83 +81,131 @@ export default class Home extends Component
       airportIndex: 0,
       shortURL: '',
       hasBadRoute: false,
-      visible: false,
-      buttonDisabled: false
+      visible: true,
+      buttonDisabled: false,
+      returnedFromAuth0: false,
+      kangarooIsHere: false
     }
     this.showNotifications = this.showNotifications.bind(this);
     this.showDrive = this.showDrive.bind(this);
     this.getFlight=this.getFlight.bind(this);
     this.toggleButton = this.toggleButton.bind(this);
+    this.dance = this.dance.bind(this)
+    this.enter = this.enter.bind(this)
+    this.leave = this.leave.bind(this)
   }
 
+  enter() {
+    console.log("Kangaroo incoming")
+    $('.loading-kangaroo').stop().animate({'left': '0vw','opacity': '1'},100,
+      function(){$(this).animate({'left': '+40vw'}, 500)})
+  }
 
-componentDidMount(){
-  axios.get("/auth/me").then(response=>{
+  dance() {
+    console.log("Kangaroo dance go")
     this.setState({
-      user:response.data
+      visible: !this.state.visible
+    })
+  }
+
+  leave() {
+    console.log("Kangaroo out")
+    $('.loading-kangaroo').stop().animate({'left': '+100vw'},500)
+  }
+
+  componentDidMount(){
+    axios.get("/auth/me").then(response=>{
+      this.setState({
+        user:response.data
+      })
+      scrollToComponent(this.Home, {
+        offset: 0,
+        align: 'bottom',
+        duration: 1000
+      })
+    })
+
+
+
+  }
+
+  toggleButton(){
+    this.setState({
+      buttonDisabled: true
+    })
+  }
+
+  showNotifications(){
+
+    this.setState({
+      displayNotifications:'block'
     })
     scrollToComponent(this.Home, {
-    offset: 0,
-    align: 'bottom',
-    duration: 1000
-  })
-  })
+      offset: 0,
+      align: 'bottom',
+      duration: 1000
+    })
 
-}
+  }
+  showDrive(){
+    this.setState({
+      driveDisplay:'block'
+    })
 
-toggleButton(){
-  this.setState({
-    buttonDisabled: true
-  })
-}
+  }
 
-handleVisibility = () => this.setState({ visible: !this.state.visible })
-showNotifications(){
+  getFlight(url){
+    console.log("This should be animating the !@#$ing kangaroo")
 
-  this.setState({
-    displayNotifications:'block'
-  })
-  scrollToComponent(this.Home, {
-    offset: 0,
-    align: 'bottom',
-    duration: 1000
-  })
-
-}
-showDrive(){
-  this.setState({
-    driveDisplay:'block'
-  })
-  
-}
-
-getFlight(url){
-  axios.get(url)
-  .then(res=> {
-    console.log("OK", res.data.directions.routes[0])
-    if((res.data.directions.routes[0].legs[0].distance.value / 1760) > 100) {
-      this.setState({
-        airportIndex: this.state.airportIndex + 1
-      })
-      axios.get(`/api/new-location/${res.data.location}/${res.data.info[0].appendix.airports[this.state.airportIndex].name}`)
-        .then(newResponse => {
-          if(newResponse.data.directions.routes.length === 0) {
-            console.log("Cannot get route!")
-            this.setState({
-              flight: '',
-              airportIndex: 0,
-              hasBadRoute: true,
-              buttonDisabled: false
+    setTimeout(this.enter,500)
+    setTimeout(this.dance, 1200)
+    setTimeout(this.dance, 2000)
+    setTimeout(this.dance, 2800)
+    setTimeout(this.dance, 3600)
+    //uncomment this to have the kangaroo leave.  Be warned, it breaks a thing
+    //setTimeout(this.leave, 4000)
+    axios.get(url)
+      .then(res=> {
+        console.log("OK", res.data.directions.routes[0])
+        if((res.data.directions.routes[0].legs[0].distance.value / 1760) > 100) {
+          this.setState({
+            airportIndex: this.state.airportIndex + 1
+          })
+          axios.get(`/api/new-location/${res.data.location}/${res.data.info[0].appendix.airports[this.state.airportIndex].name}`)
+            .then(newResponse => {
+              if(newResponse.data.directions.routes.length === 0) {
+                console.log("Cannot get route!")
+                this.setState({
+                  flight: '',
+                  airportIndex: 0,
+                  hasBadRoute: true,
+                  buttonDisabled: false
+                })
+                return
+              }
+              console.log("oldish data", res.data)
+              console.log("response from server", newResponse.data)
+              let newFlight = Object.assign({}, res.data, newResponse.data)
+              console.log("Building shortenedURL", newFlight)
+              axios.post(`https://www.googleapis.com/urlshortener/v1/url?key=${config.googleShortener}`,
+                {"longUrl": `https://www.google.com/maps/dir/Current+Location/${newFlight.directions.routes[0].legs[0].end_location.lat},${newFlight.directions.routes[0].legs[0].end_location.lng}`
+                })
+                .then(response => {
+                  console.log("shortened URL:", response.data.id)
+                  this.setState({
+                    shortURL: response.data.id
+                  })
+                })
+              this.setState({
+                flight: newFlight
+              })
+              setTimeout(this.showNotifications, 3500)
             })
-            return
-          }
-          console.log("oldish data", res.data)
-          console.log("response from server", newResponse.data)
-          let newFlight = Object.assign({}, res.data, newResponse.data)
-          console.log("Building shortenedURL", newFlight)
+        }
+        else {
+          console.log("Building shortenedURL")
           axios.post(`https://www.googleapis.com/urlshortener/v1/url?key=${config.googleShortener}`,
-            {"longUrl": `https://www.google.com/maps/dir/Current+Location/${newFlight.directions.routes[0].legs[0].end_location.lat},${newFlight.directions.routes[0].legs[0].end_location.lng}`
-            })
+            {"longUrl": `https://www.google.com/maps/dir/Current+Location/${res.data.directions.routes[0].legs[0].end_location.lat},${res.data.directions.routes[0].legs[0].end_location.lng}`})
             .then(response => {
               console.log("shortened URL:", response.data.id)
               this.setState({
@@ -163,53 +213,36 @@ getFlight(url){
               })
             })
           this.setState({
-            flight: newFlight
+            flight:res.data
           })
-          this.showNotifications()
-        })
-    }
-    else {
-      console.log("Building shortenedURL")
-      axios.post(`https://www.googleapis.com/urlshortener/v1/url?key=${config.googleShortener}`,
-        {"longUrl": `https://www.google.com/maps/dir/Current+Location/${res.data.directions.routes[0].legs[0].end_location.lat},${res.data.directions.routes[0].legs[0].end_location.lng}`})
-        .then(response => {
-          console.log("shortened URL:", response.data.id)
-          this.setState({
-            shortURL: response.data.id
-          })
-        })
-      this.setState({
-        flight:res.data
+          setTimeout(this.showNotifications, 3500)
+        }
       })
-      this.showNotifications()
-    }
-  })
-}
+  }
 
 
   render()
   {
 
-    
     let userFirstName
     if (this.state.user.name){
-        userFirstName = this.state.user.name.givenName
-      }
+      userFirstName = this.state.user.name.givenName
+    }
     const isLoggedIn = (
       <Text>
-        <Padder style={{ color: "#ff835d", fontWeight: 'bold'  }}>You're logged in, {userFirstName}. 
+        <Padder style={{ color: "#ff835d", fontWeight: 'bold'  }}>You're logged in, {userFirstName}.
           <br/>
           Let's plan your drive!
         </Padder>
         <Spacer10H>
-          <Transition.Group animation= { this.state.visible?'fly right':'fly left'} duration={500}>
-            { this.state.visible&& <LogoSmall src={logo}/>}
-          </Transition.Group>
+          <Transition animation={'bounce'} duration={500} visible={this.state.visible}>
+            <LogoSmall className="loading-kangaroo" src={logo}/>
+          </Transition>
         </Spacer10H>
-        <FlightInput ref="flightInput" user={this.state.user} 
-        show={this.showNotifications} flight={this.getFlight} 
-        buttonDisabled={this.state.buttonDisabled} toggleButton={this.toggleButton}/>
-        {/*<button onClick={this.handleVisibility}>Press me</button>*/}
+        <FlightInput ref="flightInput" user={this.state.user}
+                     show={this.showNotifications} flight={this.getFlight}
+                     buttonDisabled={this.state.buttonDisabled} toggleButton={this.toggleButton}/>
+
         <Spacer12/>
         <NotificationPref user={this.state.user}
                           flight={this.state.flight}
@@ -227,12 +260,12 @@ getFlight(url){
     )
     return (
       <div className="home" ref={(section) => { this.Home = section; }}>
-        <Logo src={logo}></Logo>
+        <Logo src={logo}/>
         <Spacer5/>
-          <Padder>
-            <Text>RooMinder gets you to the airport just in time to pick them up. Hop to it on time, every time.</Text > 
-             <Spacer10/>
-          </Padder>
+        <Padder>
+          <Text>RooMinder gets you to the airport just in time to pick them up. Hop to it on time, every time.</Text >
+          <Spacer10/>
+        </Padder>
         <Login user= {this.state.user}/>
         {this.state.user?isLoggedIn:null}
         <Modal
